f01fac4e4492d0ecbf625638867fbd9419d005bc
kierwl <45115106+kierwl@users.noreply.github.com>
2025-03-31 10:02:12 +0900

    [feat] 플레이어유닛 사망시 데이터 호출 델리게이터 추가
        // 정적 이벤트 (어디서든 접근 가능)
        public static event PlayerDeathHandler OnPlayerDeath;
    
        // 인스턴스 이벤트 (특정 유닛의 죽음만 감지하고 싶을 때 사용)
        public event PlayerDeathHandler OnThisPlayerDeath;

--- Assets/01_Scripts/PlayerUnit/PlayerUnit.cs
+++ Assets/01_Scripts/PlayerUnit/PlayerUnit.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System;
+
 public enum UnitState
 {
     Idle,
@@ -14,6 +16,18 @@ public enum UnitState
 public interface IDamageable
 {
     void TakeDamage(float damage);
+}
+
+// 사망 데이터를 담을 클래스 정의
+[Serializable]
+public class PlayerDeathData
+{
+    public Vector3 DeathPosition;
+    public Quaternion DeathRotation;
+    public UnitSO UnitData;
+    public float TimeOfDeath;
+    public Transform LastTarget;
+    public float RemainingHP;
 }
 
 public class PlayerUnit : MonoBehaviour, IDamageable
@@ -45,6 +59,15 @@ public class PlayerUnit : MonoBehaviour, IDamageable
     private UnitSkillController _skillController;
     private UnitStateController _stateController;
 
+    // 델리게이트 정의
+    public delegate void PlayerDeathHandler(PlayerDeathData deathData);
+
+    // 정적 이벤트 (어디서든 접근 가능)
+    public static event PlayerDeathHandler OnPlayerDeath;
+
+    // 인스턴스 이벤트 (특정 유닛의 죽음만 감지하고 싶을 때 사용)
+    public event PlayerDeathHandler OnThisPlayerDeath;
+
     private void Awake()
     {
         InitializeComponents();
@@ -72,7 +95,7 @@ public class PlayerUnit : MonoBehaviour, IDamageable
         // 컴포넌트 설정
         _animationController.Initialize(this);
         _attackController.Initialize(this, unitData, projectilePrefab, firePoint, enemyLayer);
-        _skillController.Initialize(this, unitData, skillEffectPrefab, manaRecoveryAmount, skillCooldown,knockbackForce,damageAmount,checkRadius);
+        _skillController.Initialize(this, unitData, skillEffectPrefab, manaRecoveryAmount, skillCooldown, knockbackForce, damageAmount, checkRadius);
         _stateController.Initialize(this, _attackController, _skillController, canUseBasicAttack, canUseSkill);
     }
 
@@ -106,6 +129,22 @@ public class PlayerUnit : MonoBehaviour, IDamageable
     {
         _currentState = UnitState.Dead;
         _animationController.TriggerDeathAnimation();
+
+        // 사망 데이터 생성
+        PlayerDeathData deathData = new PlayerDeathData
+        {
+            DeathPosition = transform.position,
+            DeathRotation = transform.rotation,
+            UnitData = unitData,
+            TimeOfDeath = Time.time,
+            LastTarget = _currentTarget,
+            RemainingHP = _currentHP
+        };
+
+        // 이벤트 발생 (null 체크)
+        OnThisPlayerDeath?.Invoke(deathData);
+        OnPlayerDeath?.Invoke(deathData);
+
         // 추가 죽음 처리 로직
         Destroy(gameObject, 1f); // 애니메이션 재생 시간 고려
     }
@@ -171,4 +210,5 @@ public class PlayerUnit : MonoBehaviour, IDamageable
     public void SetCurrentTarget(Transform target) => _currentTarget = target;
     public UnitAnimationController GetAnimationController() => _animationController;
     public UnitSO GetUnitData() => unitData;
+    public float GetCurrentHP() => _currentHP;
 }
