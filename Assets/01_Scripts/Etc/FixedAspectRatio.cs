6e23966bab7c992cc93093a97ac0d9317f00e227
dalsi <tkdwns8632@gmail.com>
2025-04-02 11:48:45 +0900

    Chore: Main, Start 씬 UI 일부 수정 및 카메라 크기 설정

---
+++ Assets/01_Scripts/Etc/FixedAspectRatio.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedAspectRatio : MonoBehaviour
+{
+    private void Start()
+    {
+        SetAspectRatio(16f / 9f);
+    }
+
+    private void SetAspectRatio(float targetAspect)
+    {
+        Camera cam = GetComponent<Camera>();
+
+        float screenAspect = (float)Screen.width / Screen.height;
+        float scaleHeight = screenAspect / targetAspect;
+
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = cam.rect;
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+            cam.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+            Rect rect = cam.rect;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+            cam.rect = rect;
+        }
+    }
+}
