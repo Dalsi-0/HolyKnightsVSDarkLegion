2c86c3a4a616977fa3d1a4cb819e45ba97269380
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2025-04-02 09:30:07 +0900

    Feat: 팝업 UI 연결 및 사운드 매니저 순서 설정

--- Assets/01_Scripts/UI/Deck/DeckEditor.cs
+++ Assets/01_Scripts/UI/Deck/DeckEditor.cs
@@ -59,7 +59,7 @@ public class DeckEditor : MonoBehaviour
         // 사용 가능 갱신
         UnitManager.Instance.ChangeMoney(0);
         // 사운드 재생
-        SoundManager.Instance.SetSfx(1);
+        SoundManager.Instance.SetSfx(4);
     }
     public void SetActive(bool active)
     {
